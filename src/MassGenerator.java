8fb509f5dee6f5cd1a34a61c7b1945ed83f85c2c
Shawn Beaudoin <shawnpbeaudoin@gmail.com>
2015-02-10 15:09:39 -0500

    Updated Minutes/Generator
    Uploaded the most recent minutes, now in text form! Mock exam for Alyssa
    to test her interpreter unit. Mass Generator used to generate a more
    meaningful exam.

--- src/MassGenerator.java
+++ src/MassGenerator.java
@@ -10,11 +10,13 @@ import javax.imageio.ImageIO;
 public class MassGenerator {
 	
 	public static void main(String args[]) throws FileNotFoundException{
-	
-//		for (int i=0; i<100; i++){
-//			QRCodeHandler.GenerateBarCode(i + ".gif", "21456" + i + "", 58);
-//		}
-	//	testCreation("");
+		int i = 0;
+		for (int student =0; student<5; student++)
+			for (int q=0; q<6; q++)
+					for (int r=0; r<5; r++)
+							QRCodeHandler.GenerateQRCode(i++ + ".gif", "v0e1s"+student+"q"+q+"a"+r +"", 58);
+			
+		testCreation("");
 	testLoad("58pxwrite.bmp");
 
 	}
@@ -28,16 +30,24 @@ public class MassGenerator {
 			e.printStackTrace();
 			return;
 		}
+		int i=0;
 		output.println("<html><body><table>");
-		for (int i=0; i<100; i++){
-			if (i%6==0){
-				output.println("<tr><td colspan=2>All right. So here is a question and some shit. Yeah I'm fucking awesome.</td></tr>");
+		for (int student =0; student<5; student++)
+			for (int q=0; q<6; q++){
+					output.println("<tr><td colspan=2> Here is Question " + q + " for Student " + student + "</td><tr>");
+					for (int r=0; r<5; r++)
+						output.println("<tr><td><img src=\"" + i++ + ".gif\"></img> </td><td>Response " + r + " .</td></tr>");
 			}
-			output.println("<tr><td><img src =\"" + i + ".gif\"></img> </td><td>Here is question number " + i + " .</td></tr>");
-//			if (i%6==0)
-//				output.println("</tr><tr>");
-//			output.println(/*"<td>" + i + ".*/"<td> <img src =\"" + i + ".gif\"></img> </td>");
-		}
+						
+//		for (int i=0; i<150; i++){
+//			if (i%6==0){
+//				output.println("<tr><td colspan=2>All right. So here is a question and some shit. Yeah I'm fucking awesome.</td></tr>");
+//			}
+//			output.println("<tr><td><img src =\"" + i + ".gif\"></img> </td><td>Here is question number " + i + " .</td></tr>");
+////			if (i%6==0)
+////				output.println("</tr><tr>");
+////			output.println(/*"<td>" + i + ".*/"<td> <img src =\"" + i + ".gif\"></img> </td>");
+//		}
 		output.println("</table></body></html>");
 		output.close();
 		System.out.println("Done");
