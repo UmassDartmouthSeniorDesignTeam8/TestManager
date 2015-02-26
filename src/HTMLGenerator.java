35b5c839b300c17f6e04d781a93b5ee4af54917c
Shawn Beaudoin <shawnpbeaudoin@gmail.com>
2015-02-26 13:56:47 -0500

    HTMLGenerator Phase 1 Complete

--- src/HTMLGenerator.java
+++ src/HTMLGenerator.java
@@ -47,21 +47,25 @@ public class HTMLGenerator {
 		int numStudents = exam.getCourse().getNumStudents();
 		//Output html file header
 		String pageBuilder = getPageHTMLHeader();
-		pageBuilder += getCoverPage();
+		pageBuilder+="<body>";
 		questions = exam.getQuestionArray();
-		for (int i=0; i<questions.length; i++){
-			pageBuilder += getQuestionHTML(questions[i]);
+		for (int student = 0; student<numStudents; student++){
+			pageBuilder += getCoverPage();
+			for (int questionNum=0; questionNum<questions.length; questionNum++)
+				pageBuilder += getQuestionHTML(questions[questionNum], questionNum, student);
+			
+			
 		}
 		
 		/*
-		 * This block generates the QR codes, which are named as their string contents. Eventually this should be delegated to
+		 * This block generates the QR codes, which are named s###q###i###. Eventually this should be delegated to
 		 * a thread.
 		 */
 		for (int s=0; s<numStudents; s++){
 			for (int q=0; q<questions.length; q++)
 				if (questions[q] instanceof MultipleChoiceQuestion){
 					for (int i=0; i<((MultipleChoiceQuestion) questions[q]).getNumChoices(); i++){
-						String fileName = String.format("%03d%03d%03d.gif", s, q, i);
+						String fileName = getQRCodePath(s, q, i);
 						QRCodeHandler.generateQRCode("1", exam.getExamID(), s, q, i,  fileName, 58);
 					}
 				}
@@ -84,11 +88,34 @@ public class HTMLGenerator {
 		return "";
 	}
 	
-	private static String getQuestionHTML(Question q){
+	private static String getQuestionHTML(Question q, int question, int student){
+		if (q instanceof MultipleChoiceQuestion)
+			return getMultipleChoiceQuestionHTML((MultipleChoiceQuestion)q, question, student);
 		return "";
 	}
 	
-	private static String getMultipleChoiceQuestionHTML(Question q){
-		return " ";
+	private static String getMultipleChoiceQuestionHTML(MultipleChoiceQuestion q, int questionNum, int student){
+		// First write the question frame, question text Frame and question text
+		String working = "<div class = \"questionFrame multipleChoiceQuestionFrame\">\n" +
+						 "<div class = \"questionTextFrame multipleChoiceQuestionTextFrame\">\n" +
+						 "<p class = \"questionText multipleChoiceQuestionText\">\n" +
+						 "<em class = \"questionNumber\">" + questionNum + ".</em>" +
+						 questions[questionNum].getQuestionText() + 
+						 "<em class = \"pointValue\">(" + questions[questionNum].getPointValue() + " points)</em></p>\n</div>";
+		String[] choices = q.getChoices();
+		// Create frames for all the responses
+		for (int i=0; i<choices.length; i++){
+			working+="<div class=\"responseFrame multipleChoiceResponseFrame\">\n" +
+					 "<div class = \"responseImageFrame\"><img class = \"qrCode\" src = \"" + getQRCodePath(student, questionNum, i) + "\"/></div>\n" +
+					"<div class =\"responseText\"><p class=\"hangingIndent\"><em class = \"responseLetter multipleChoiceResponseLetter\">" + ('A' + i) +
+					"</em>" + choices[i] + "</p>\n</div>";
+		}
+		// End frame after all responses have been populated
+		working += "</div>";
+		return working;
+	}
+	
+	private static String getQRCodePath(int student, int question, int response){
+		return String.format("images/s%03dq%03di%03d.gif", student, question, response);
 	}
 }
